b4c13d594e5cd46772f6e00cb3619d734784b71f
Eapen Mani <eapenmani@gmail.com>
2023-03-08 11:41:45 +0530

    Added the script to create S3 bucket

--- provider.tf
+++ provider.tf
@@ -1,16 +1,6 @@
 provider "aws" {
   region = var.region
 }
-resource "aws_s3_bucket" "b" {
-  bucket = "terraform-eapenmani-kottayam"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "DevJKT"
-  }
-}
-
 terraform {
   backend "s3" {
     bucket = "terraform-eapenmani-kottayam"
