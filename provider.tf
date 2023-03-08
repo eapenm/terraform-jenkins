2ea99c9a6e21945e7d3881023525fd34f426f251
Eapen Mani <eapenmani@gmail.com>
2023-03-08 08:08:17 +0530

    Added provider

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-provider "aws" {
-  region = var.region
-}
-
 terraform {
   backend "s3" {
-    bucket = "terraform-tf-1234"
-    key    = "terraform.tfstate"
+    bucket = "terraform-eapen-1234"
+    key    = "terraform-jenkins.tfstate"
     region = "us-east-1"
   }
+}
+
+provider "aws" {
+  region = var.region
 }
