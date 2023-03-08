d3129f05d869b7345dd264f3566e627969b9ac4b
Eapen Mani <eapenmani@gmail.com>
2023-03-08 08:21:38 +0530

    Fixed

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-eapen-1234"
-    key            = "terraform-jenkins.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "devops-tf-state-lock"
-  }
-}
-
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-eapen-1234"
+    key    = "terraform-jenkins.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
